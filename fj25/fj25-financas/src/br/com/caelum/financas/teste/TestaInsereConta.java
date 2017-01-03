cf450f2cf422e503009de754c0e35c33e133e996
João <jsoft-ti@usp.br>
2017-01-02 22:39:27 -0200

    Exercício 4.2

--- fj25/fj25-financas/src/br/com/caelum/financas/teste/TestaInsereConta.java
+++ fj25/fj25-financas/src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -5,13 +5,15 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TestaInsereConta {
 	public static void main(String[] args) {
 		long inicio = System.currentTimeMillis();
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
-		EntityManager manager = factory.createEntityManager();
+		//EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
+		//EntityManager manager = factory.createEntityManager();
 		
+		EntityManager manager = new JPAUtil().getEntityManager();
 		Conta conta = new Conta();
 		conta.setTitular("João Carlos");
 		conta.setBanco("Safra");
